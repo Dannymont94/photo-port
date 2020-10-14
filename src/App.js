4ff0166148868d779a1a33dcf2a140db99b3f63f
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-10-14 18:22:38 -0400

    created Contact component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import Contact from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -29,10 +30,9 @@ function App() {
     <div>
       <Nav categories={categories} setCurrentCategory={setCurrentCategory} currentCategory={currentCategory}></Nav>
       <main>
-        <div>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
-        </div>
+        <Contact></Contact>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
